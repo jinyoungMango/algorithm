23b910b3fed48ec3da6f3309a9fb10e0c022c7ce
jinyoungMango <123930169+jinyoungMango@users.noreply.github.com>
2023-02-08 22:58:20 +0900

    2023.02.08

---
+++ Baekjoon/p2587.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class p2587 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[5];
+		
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		
+		int avg = sum / arr.length;
+		
+		Arrays.sort(arr);
+		
+		System.out.println(avg);
+		System.out.println(arr[2]);
+	}
+}
